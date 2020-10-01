33f4c7ab83f079ff048ecadb5b1c5b50ec116543
1139195320 <fy1139195320@qq.com>
2020-10-01 20:50:42 +0800

    1

--- src/com/jack/algorithm/problem/easy/LeetCode10501.java
+++ src/com/jack/algorithm/problem/easy/LeetCode10501.java
@@ -17,4 +17,14 @@ public class LeetCode10501 {
         N += (M << i);
         return N;
     }
+
+    public int insertBits2(int N, int M, int i, int j) {
+        // i-j 全 1，其余全 0
+        int mask = ((1 << (j - i + 1)) - 1) << i;
+        // i-j 全 0，其余全 1
+        mask = ~mask;
+        // i-j 的 N 清零
+        N &= mask;
+        return N | (M << i);
+    }
 }
