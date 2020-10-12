6b6103df08a45cd0d6424b803ab9cdb07315a71b
fangyang <fy1139195320@qq.com>
2020-10-12 09:39:17 +0800

    二叉搜索树的最小绝对差

---
+++ src/com/jack/algorithm/problem/easy/LeetCode0530.java
@@ -0,0 +1,32 @@
+package com.jack.algorithm.problem.easy;
+
+/**
+ * @author jack
+ */
+public class LeetCode0530 {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+    }
+
+    private int pre, res;
+    public int getMinimumDifference(TreeNode root) {
+        res = Integer.MAX_VALUE;
+        dfs(root);
+        return res;
+    }
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        if (pre != -1) {
+            res = Math.min(res, node.val - pre);
+        }
+        pre = node.val;
+        dfs(node.right);
+    }
+}
